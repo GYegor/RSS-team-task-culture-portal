4d049005e4431f9e6f1dc3c1f560089c3a6204d2
Egor Glushanko <e.glushanko@realt.by>
2022-11-14 18:41:41 +0300

    og meta

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,8 +34,8 @@ class Header extends Component {
     console.log(this.props);
     if (navigator.share) {
       navigator.share({
-        title: 'Yo Bro! Check this out!',
-        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets",
+        // text: 'Yo Bro! Check this out!',
+        title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets\n",
         url: window.location.href,
       });
     } else {
